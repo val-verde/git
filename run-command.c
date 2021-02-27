02094c1bf420fa93c15fc612d5cd075f624f8ca0
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-02-27 14:35:38 -0800

    Add android implementation for pthread_setcancel/pthread_setcancelstate.

--- run-command.c
+++ run-command.c
@@ -9,6 +9,37 @@
 #include "quote.h"
 #include "config.h"
 
+#ifdef __ANDROID__
+
+#define SIG_CANCEL_SIGNAL SIGUSR1
+#define PTHREAD_CANCEL_ENABLE 1
+#define PTHREAD_CANCEL_DISABLE 0
+
+typedef long pthread_t;
+
+static int pthread_setcancelstate(int state, int *oldstate)
+{
+	sigset_t new, old;
+	int ret;
+
+	sigemptyset(&new);
+	sigaddset(&new, SIG_CANCEL_SIGNAL);
+	ret = pthread_sigmask(state == PTHREAD_CANCEL_ENABLE ? SIG_BLOCK : SIG_UNBLOCK, &new , &old);
+
+	if (oldstate != NULL) {
+	    *oldstate =sigismember(&old,SIG_CANCEL_SIGNAL) == 0 ? PTHREAD_CANCEL_DISABLE : PTHREAD_CANCEL_ENABLE;
+	}
+
+	return ret;
+}
+
+static inline int pthread_cancel(pthread_t thread)
+{
+	return pthread_kill(thread, SIG_CANCEL_SIGNAL);
+}
+
+#endif
+
 void child_process_init(struct child_process *child)
 {
 	struct child_process blank = CHILD_PROCESS_INIT;
